8c320a97fe58a959ce9a924e32d63dad2619e1a3
CarsonWhiteocc <98561349+CarsonWhiteocc@users.noreply.github.com>
2024-03-30 21:46:33 -0400

    First "Complete" Version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # senior_project
- 
+[pip install pillow] to install library at command line
+python main.py to run
+go to http://localhost:8080/ to see local website
+only use pngs for image file
--- main.py
+++ main.py
@@ -2,6 +2,125 @@
 from PIL import Image
 # Random for key creation
 import random
+import zipfile
+
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import cgi
+import os
+
+# Define the hostname and port
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Send a 200 OK response
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        # Check the requested path and respond accordingly
+        if self.path == "/":
+            # UI with a link to the About page
+            self.wfile.write(bytes("<html><head><title>Image encrypter & decrypter web serverr</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            self.wfile.write(bytes("<h1>Encode & Decode Images Here</h1>", "utf-8"))
+            self.wfile.write(bytes("<p><a href='/encode'>encode</a></p>", "utf-8"))
+            self.wfile.write(bytes("<p><a href='/decode'>decode</a></p>", "utf-8"))
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+        elif self.path == "/encode":
+            # About page
+            self.wfile.write(bytes("<html><head><title>Encode</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body><h1>Encode Page</h1>", "utf-8"))
+            self.wfile.write(b"<form method='POST' enctype='multipart/form-data'>")
+            self.wfile.write(b"<input type='file' name='img_file'><br><p>Image</p>")
+            self.wfile.write(b"<input type='submit' value='Upload'>")
+            self.wfile.write(b"</form>")
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+        elif self.path == "/decode":
+            # About page
+            self.wfile.write(bytes("<html><head><title>Decode</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body><h1>Decode Page</h1>", "utf-8"))
+            self.wfile.write(b"<form method='POST' enctype='multipart/form-data'>")
+            self.wfile.write(b"<input type='file' name='img_file'><p>Image</p><br>")
+            self.wfile.write(b"<input type='file' name='key_file'><p>Key</p><br>")
+            self.wfile.write(b"<input type='submit' value='Upload'>")
+            self.wfile.write(b"</form>")
+            self.wfile.write(bytes("</body></html>", "utf-8"))
+        else:
+            # 404 - Page not found
+            self.wfile.write(bytes("<html><head><title>Not Found</title></head>", "utf-8"))
+            self.wfile.write(bytes("<body><p>404 - Page not found.</p></body></html>", "utf-8"))
+
+    def do_POST(self):
+        # Handle POST requests (file upload)
+        if self.path == "/decode":
+            form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={"REQUEST_METHOD": "POST"}
+            )
+            img_file = form["img_file"]
+            key_file = form['key_file']
+            if img_file.filename:
+                # Save the uploaded file (customize the path as needed)
+                img_path = os.path.join(os.getcwd(), img_file.filename)
+                key_path = os.path.join(os.getcwd(), key_file.filename)
+                with open(img_path, "wb") as f:
+                    f.write(img_file.file.read())
+                with open(key_path, "wb") as f:
+                    f.write(key_file.file.read())
+                decrypt_image(img_path, key_path)
+                with open(img_path, "rb") as f:
+                    self.send_response(200)
+                    self.send_header("Content-type", "application/zip")
+                    self.send_header("Content-Disposition", "attachment; filename=" + img_file.filename)
+                    self.end_headers()
+                    self.wfile.write(f.read())
+                os.remove(key_path)
+                os.remove(img_path)
+            else:
+                self.send_response(400)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                self.wfile.write(b"<html><body>")
+                self.wfile.write(b"<p>Error uploading file.</p>")
+                self.wfile.write(b"</body></html>")
+        elif self.path == "/encode":
+            form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={"REQUEST_METHOD": "POST"}
+            )
+            img_file = form["img_file"]
+            if img_file.filename:
+                # Save the uploaded file (customize the path as needed)
+                img_path = os.path.join(os.getcwd(), img_file.filename)
+                with open(img_path, "wb") as f:
+                    f.write(img_file.file.read())
+                encode_image(img_path, 'key.txt')
+                file_names = [img_file.filename, "key.txt"]  # Customize with your file names
+                with zipfile.ZipFile("key_img.zip", "w") as zipf:
+                    for file_name in file_names:
+                        file_path = os.path.join(os.getcwd(), file_name)
+                        if os.path.exists(file_path):
+                            zipf.write(file_path, file_name)
+                with open("key_img.zip", "rb") as f:
+                    self.send_response(200)
+                    self.send_header("Content-type", "application/zip")
+                    self.send_header("Content-Disposition", "attachment; filename=test.zip")
+                    self.end_headers()
+                    self.wfile.write(f.read())
+                os.remove(img_path)
+                os.remove('key.txt')
+                os.remove('key_img.zip')
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"<html><body><p>404 - Page not found.</p></body></html>")
 
 
 # Takes an image path and a path to place the key
@@ -69,23 +188,16 @@ def decrypt_image(path, key_path):
     input.save(path, format='png')
 
 
-# Main
-if __name__ == '__main__':
-    # Menu loop
-    while(True):
-        # Choices
-        i = input('1. encrypt 2. decrypt 3. quit\n')
-        match i:
-            # Encrypt
-            case '1':
-                path = input('Enter path of unencrypted image: ')
-                key = input('Enter key location: ')
-                encode_image(path, key)
-            # Decrypt
-            case '2':
-                path = input('Enter path of encrypted image: ')
-                key = input('Enter key location: ')
-                decrypt_image(path, key)
-            # Exit
-            case '3':
-                break
+if __name__ == "__main__":
+    hostName = "localhost"
+    serverPort = 8080
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print(f"Server started at http://{hostName}:{serverPort}")
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
